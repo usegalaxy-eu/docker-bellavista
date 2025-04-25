ff41caf4a9d1f6f9902520fe704d3b65a2d1e688
Amirhossein Naghsh Nilchi <amirnnilchi@gmail.com>
2025-04-25 17:46:40 +0200

    change if logic

--- startapp.sh
+++ startapp.sh
@@ -2,9 +2,8 @@
 
 . /opt/bellavista/bellavista/bin/activate
 
-if [ -z ${BELLAVISTA_DIR+x} ]
-  then
-      cd $BELLAVISTA_DIR && bellavista "./config.json"
-  else
-      cd /input && bellavista "./config.json"
+if [ -n "${BELLAVISTA_DIR}" ]; then
+    cd "$BELLAVISTA_DIR" && bellavista "./config.json"
+else
+    cd /input && bellavista "./config.json"
 fi
